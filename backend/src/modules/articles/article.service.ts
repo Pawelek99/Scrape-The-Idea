d926ee4987b53308a51a558c0e1b22d285c39aff
KonradWrbl <160815@stud.prz.edu.pl>
2020-07-04 11:25:34 +0200

    dribble thumbnail scrapping

--- backend/src/modules/articles/article.service.ts
+++ backend/src/modules/articles/article.service.ts
@@ -3,6 +3,7 @@ import { Article } from './schemas/article.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import fetch from 'node-fetch';
+const cheerio = require('cheerio');
 
 @Injectable()
 export class ArticlesService {
@@ -11,6 +12,25 @@ export class ArticlesService {
     async findRecent(): Promise<Article[]> {
         const res = await fetch('https://dribbble.com/?page=1&per_page=24');
         const body = await res.text();
+
+        console.log('fasad')
+        const _ = cheerio.load(body);
+        let data = []
+        for(let i = 0; i < 20; i++) {
+            data.push({
+                //@temporary id
+                id: Math.floor(Math.random() * 100**10),
+                author: _('.display-name')[`${i}`].children[0].data,
+                title: _('.shot-title')[`${i}`].children[0].data,
+                thumbnail: _('picture').children('img')[`${i}`].attribs.src,
+                link: 'https://www.dribbble.com'+_('.dribbble-link')[`${i}`].attribs.href,
+                website: 'DRIBBLE',
+                createdAt: Date.now(),
+                //@todo
+                stars: '1000',
+            })
+        }
+        console.log(data)
 
         const regex = /var newestShots = ((?:.|\n)*?\}])/m;
         const m = regex.exec(body);
